86bf98d3bc6ac88f4dd545571de2610213833b15
Sultanov Albert <sultanovalbertrash@gmail.com>
2016-04-15 13:07:35 +0300

    add .travis.yml

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,9 +1,3 @@
 <?php
 // This is global bootstrap for autoloading
 include __DIR__.'/../vendor/autoload.php'; // composer autoload
-
-$kernel = \AspectMock\Kernel::getInstance();
-$kernel->init([
-    'debug' => true,
-    'includePaths' => [__DIR__.'/../src']
-]);
